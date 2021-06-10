df09808e05a547be100713c593a769bf7a2caed0
hazthegoat <hazaraskari@gmail.com>
2021-06-10 23:41:46 +0200

    restructuring app folder structure

---
+++ pages/NewPage.tsx
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+function NewPage({ Component, pageProps }: AppProps) {
+  return <h1>Hello new page</h1>;
+}
+export default NewPage;
