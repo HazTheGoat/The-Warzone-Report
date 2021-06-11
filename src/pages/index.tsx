49f9cd09d28cce7f9ede1dedc799c0bb5f0100d4
hazthegoat <hazaraskari@gmail.com>
2021-06-11 20:47:32 +0200

    adding callofduty api nm packages

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+
+export default function Home() {
+  console.log();
+  return <h1>Apex stats</h1>;
+}
