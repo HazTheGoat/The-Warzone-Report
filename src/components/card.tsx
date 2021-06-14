17ce2220d9d056124dd97d668a834955945dbd3d
hazthegoat <hazaraskari@gmail.com>
2021-06-14 15:35:11 +0200

    addiing some logic

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,54 +1,52 @@
-import Image from "next/image"
+import Image from "next/image";
 
-const Card = ({user, counter}:any) => {
-    console.log("test", user);
-    
-    return ( 
-      <div className="card">
-        <div className="card-header">
-            <Image width="90" height="120" src={`/avatar-pictures/${user.avatar}.png`} className="card-Image-top" alt="..."/>
-            <div className="header-text">
-                <h3>{(user.data.kdRatio).toFixed(2)}</h3>
-                <p>K/D Ratio </p>
-                {
-                  user.hasOwnProperty("positiveWeeklyKD") ? 
-                  <p>K/D arrow {user.positiveWeeklyKD ? "Up" : "Down"}</p> : null
-                  }
-            </div>
-            <h5 className="card-title">{user.username}</h5>
-        </div>
-            <div className="card-body">
-                <div className="card-stats-body">
-                    <div className="inner-stats-body">
-                      <div className="text-box">
-                        <p>{(user.data.wins)}</p>
-                        <p className="text-muted">Wins</p>
-                      </div>
-                      <div className="text-box">
-                        <p>{(user.data.topFive)}</p>
-                        <p className="text-muted">Top Five</p>
-                      </div>
-                    </div>
-                    <div className="inner-stats-body">
-                      <div className="text-box">
-                      <p>{(user.data.gamesPlayed)}</p>
-                      <p className="text-muted">Games</p>
-                      </div>
-                      <div className="text-box">
-                        <p> {((user.data.accuracy) *100).toFixed(2)}%</p>
-                        <p className="text-muted">Accuracy</p>
-                      </div>
-                      { (user.data.gulagKills ? <div className="text-box">
-                        <p> {(user.data.gulagKills)}</p>
-                        <p className="text-muted">Gulag</p>
-                      </div> : null) 
-                      }
-                    </div>
-                </div>
-            </div>
+const Card = ({ user, counter }: any) => {
+  console.log(user);
+  return (
+    <div className="apex-card">
+      <div className="avatar">
+        <Image
+          width="160"
+          height="250"
+          layout={"fixed"}
+          objectFit={"contain"}
+          src={`/avatar-pictures/${user.avatar}.png`}
+          alt="..."
+        />
       </div>
-     );
-}
- 
-export default Card;
+      <div>
+        <h3>{user.data.kdRatio.toFixed(2)}</h3>
+        <p>K/D Ratio </p>
+        {user.hasOwnProperty("positiveWeeklyKD") ? (
+          <p>K/D arrow {user.positiveWeeklyKD ? "Up" : "Down"}</p>
+        ) : null}
+      </div>
+      <h5>{user.username}</h5>
+      <h5>{user.rank}</h5>
+      <div>
+        <p>{user.data.wins}</p>
+        <p>Wins</p>
+      </div>
+      <div>
+        <p>{user.data.topFive}</p>
+        <p>Top Five</p>
+      </div>
+      <div>
+        <p>{user.data.gamesPlayed}</p>
+        <p>Games</p>
+      </div>
+      <div>
+        <p> {(user.data.accuracy * 100).toFixed(2)}%</p>
+        <p>Accuracy</p>
+      </div>
+      {user.data.gulagKills ? (
+        <div>
+          <p> {user.data.gulagKills}</p>
+          <p>Gulag</p>
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
+export default Card;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
 import { usersToFetch } from "../constants/username";
-import Card from "../components/card"
+import Card from "../components/card";
 
 interface user {
   username: string;
   avatar: string;
   weekly: any;
   lifetime: any;
+}
 
+enum CardType {
+  "wood" = 0.5, // < .5
+  "iron" = 0.7, // < .7
+  "bronze" = 0.8, // < .8
+  "silver" = 0.9, // < .9
+  "gold" = 1, // 1
+  "platinum" = 1.3, // 1.3
+  "diamond" = 1.5, // 1.5
+  "master" = 1.7, // 1.7
+  "challenger" = 2, // 2
+  "god" = 3, // 3
 }
 
 const Home = ({ fetchedUsers }: any) => {
@@ -17,29 +29,90 @@ const Home = ({ fetchedUsers }: any) => {
     setUsers(JSON.parse(fetchedUsers));
   }, []);
 
-  useEffect(() => {
-    console.log(users);
-  }, [users]);
+  useEffect(() => {}, [users]);
+
+  const getRank = (kd: any) => {
+    console.log({ kd, enum: CardType.platinum });
+    console.log(CardType[CardType.platinum]);
+
+    switch (true) {
+      case kd < CardType.wood:
+        return CardType[CardType.wood];
+
+      case kd < CardType.bronze && kd > CardType.wood:
+        return CardType[CardType.iron];
+
+      case kd < CardType.silver && kd > CardType.iron:
+        return CardType[CardType.bronze];
+
+      case kd < CardType.gold && kd > CardType.bronze:
+        return CardType[CardType.silver];
+
+      case kd < CardType.platinum && kd > CardType.silver:
+        return CardType[CardType.gold];
+
+      case kd < CardType.diamond && kd > CardType.gold:
+        return CardType[CardType.platinum];
+
+      case kd < CardType.master && kd > CardType.platinum:
+        return CardType[CardType.diamond];
+
+      case kd < CardType.challenger && kd > CardType.diamond:
+        return CardType[CardType.master];
+
+      case kd < CardType.god && kd > CardType.master:
+        return CardType[CardType.challenger];
+
+      case kd > CardType.god:
+        return CardType[CardType.god];
+
+      default:
+        return CardType[CardType.gold];
+    }
+  };
 
   return (
-    <div className="container">
-      <div className="container-fluid">
-      <h1 className="text-center">Lifetime</h1>
+    <div className="container-fluid">
+      <div className="">
+        <h1 className="text-center">Lifetime</h1>
         <div className="row align-items-start">
-          <div className="col">
-            {users.sort((a,b) => b.lifetime.kdRatio-a.lifetime.kdRatio).map((user, i) => {
-              const mappedUser = {data: user.lifetime, username: user.username, avatar: user.avatar}
-              return (<Card user={mappedUser} key={i} /> )
+          {users
+            .sort((a, b) => b.lifetime.kdRatio - a.lifetime.kdRatio)
+            .map((user, i) => {
+              const mappedUser = {
+                data: user.lifetime,
+                username: user.username,
+                avatar: user.avatar,
+                rank: getRank(user.lifetime.kdRatio),
+              };
+              return (
+                <div className="col" key={i}>
+                  <Card user={mappedUser} />
+                </div>
+              );
             })}
-          </div>
         </div>
       </div>
       <div className="container-fluid">
         <h1 className="text-center">Weekly</h1>
-        {users.sort((a,b) => b.weekly.kdRatio-a.weekly.kdRatio).map((user, i) => {
-          const mappedUSer = {data: user.weekly, username: user.username, avatar: user.avatar, positiveWeeklyKD: user.weekly.kdRatio > user.lifetime.kdRatio}
-          return (<Card user={mappedUSer}  key={i}/> )
-        })}
+        <div className="row align-items-start">
+          {users
+            .sort((a, b) => b.weekly.kdRatio - a.weekly.kdRatio)
+            .map((user, i) => {
+              const mappedUSer = {
+                data: user.weekly,
+                username: user.username,
+                avatar: user.avatar,
+                positiveWeeklyKD: user.weekly.kdRatio > user.lifetime.kdRatio,
+                rank: getRank(user.weekly.kdRatio),
+              };
+              return (
+                <div className="col" key={i}>
+                  <Card user={mappedUSer} />
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
@@ -52,8 +125,7 @@ export async function getStaticProps() {
   const mappedUsers = await usersToFetch.map(async (user) => {
     try {
       let data = await API.MWwz(user.username, user.platform);
-      console.log("sjekker data", data.weekly);
-      
+
       const {
         weekly: {
           all: { properties },
@@ -81,14 +153,12 @@ export async function getStaticProps() {
           avgLifeTime,
         },
         username: user.name,
-        avatar: user.avatar
+        avatar: user.avatar,
       };
     } catch (error) {}
   });
 
-  
   const fetchedUsersAsJSON = await Promise.all(mappedUsers).then((x) =>
-
     JSON.stringify(x)
   );
 
