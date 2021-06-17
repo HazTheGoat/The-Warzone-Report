2ecfb6ea40567ff9098c16917739a5b32ba680c4
hazthegoat <hazaraskari@gmail.com>
2021-06-17 11:35:57 +0200

    adding GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,29 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
-import 'bootstrap/dist/css/bootstrap.css';
- import "../../styles/bootstrap-overrides.css";
-
+import "bootstrap/dist/css/bootstrap.css";
+import "../../styles/bootstrap-overrides.css";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
