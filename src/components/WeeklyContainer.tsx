efcc12ae0608873724739511e0709db2dc9e5f2a
hazthegoat <hazaraskari@gmail.com>
2021-06-18 23:26:39 +0200

    .

--- src/components/WeeklyContainer.tsx
+++ src/components/WeeklyContainer.tsx
@@ -1,4 +1,4 @@
-import Card from "./Card";
+import Card from "./card";
 import { User } from "../types/types";
 import ScrollInformation from "./ScrollInformation";
 
@@ -7,7 +7,7 @@ const WeeklyContainer = ({ users }: any) => {
     <div className="d-flex justify-content-start align-items-center ">
       <ScrollInformation periodText="Weekly"></ScrollInformation>
 
-      {users.map((user: any, i: number) => (
+      {users.map((user: User, i: number) => (
         <div key={i}>
           <Card user={user} />
         </div>
