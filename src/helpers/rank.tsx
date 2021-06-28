180472608bd6e98762cc87d438a5d193d87d8531
hazthegoat <hazaraskari@gmail.com>
2021-06-28 20:42:44 +0200

    god card styling

--- src/helpers/rank.tsx
+++ src/helpers/rank.tsx
@@ -5,31 +5,31 @@ export const getRank = (kd: any) => {
     case kd < CardType.iron:
       return CardType[CardType.wood];
 
-    case kd < CardType.bronze && kd > CardType.wood:
+    case kd < CardType.bronze && kd >= CardType.wood:
       return CardType[CardType.iron];
 
-    case kd < CardType.silver && kd > CardType.iron:
+    case kd < CardType.silver && kd >= CardType.iron:
       return CardType[CardType.bronze];
 
     case kd < CardType.gold && kd > CardType.bronze:
       return CardType[CardType.silver];
 
-    case kd < CardType.platinum && kd > CardType.silver:
+    case kd < CardType.platinum && kd >= CardType.silver:
       return CardType[CardType.gold];
 
-    case kd < CardType.diamond && kd > CardType.gold:
+    case kd < CardType.diamond && kd >= CardType.gold:
       return CardType[CardType.platinum];
 
-    case kd < CardType.master && kd > CardType.platinum:
+    case kd < CardType.master && kd >= CardType.platinum:
       return CardType[CardType.diamond];
 
-    case kd < CardType.challenger && kd > CardType.diamond:
+    case kd < CardType.challenger && kd >= CardType.diamond:
       return CardType[CardType.master];
 
-    case kd < CardType.god && kd > CardType.master:
+    case kd < CardType.god && kd >= CardType.master:
       return CardType[CardType.challenger];
 
-    case kd > CardType.god:
+    case kd >= CardType.god:
       return CardType[CardType.god];
 
     default:
