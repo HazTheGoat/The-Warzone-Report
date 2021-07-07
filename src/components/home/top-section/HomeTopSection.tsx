4168bc9d50603deb003fb621ba33fe4173c9ce0c
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2021-07-07 08:44:12 +0200

    restructure

--- src/components/home/top-section/HomeTopSection.tsx
+++ src/components/home/top-section/HomeTopSection.tsx
@@ -2,80 +2,9 @@ import { Button, Grid } from "@material-ui/core";
 import { Container } from "@material-ui/core";
 import React from "react";
 import Card from "../../card";
+import { users } from "../../../constants/promo-username";
 
 const HomeTopSection = () => {
-  const users = [
-    {
-      avatar: "HazTheGoat",
-      badges: ["pitbull"],
-      data: {
-        gulagKills: 44,
-        weeklyAssists: 152,
-        weeklyAvgLifeTime: 790.8953068592058,
-        weeklyDamageDone: 219719,
-        weeklyDamageTaken: 49748,
-        weeklyDeaths: 185,
-        weeklyDistanceTraveled: 31669887.771000005,
-        weeklyHeadshotPercentage: 0.24155405405405406,
-        weeklyKdRatio: 3.18,
-        weeklyKills: 620,
-        weeklyKillsPerGame: 6.434782608695652,
-        weeklyMatchesPlayed: 90,
-        weeklyWallbangs: 0,
-      },
-      rank: "god",
-      username: "HazTheGoat",
-      weeklyKdRatioTrend: 1,
-      dateOfBirth: "1985-01-02",
-    },
-    {
-      avatar: "SuperKriss",
-      badges: ["martyr"],
-      data: {
-        gulagKills: 44,
-        weeklyAssists: 152,
-        weeklyAvgLifeTime: 790.8953068592058,
-        weeklyDamageDone: 219719,
-        weeklyDamageTaken: 49748,
-        weeklyDeaths: 185,
-        weeklyDistanceTraveled: 31669887.771000005,
-        weeklyHeadshotPercentage: 0.24155405405405406,
-        weeklyKdRatio: 2.65,
-        weeklyKills: 450,
-        weeklyKillsPerGame: 6.434782608695652,
-        weeklyMatchesPlayed: 110,
-        weeklyWallbangs: 0,
-      },
-      rank: "diamond",
-      username: "SuperKriss",
-      weeklyKdRatioTrend: -1,
-      dateOfBirth: "1993-06-21",
-    },
-    {
-      avatar: "Anders1337",
-      badges: ["deadeye"],
-      data: {
-        gulagKills: 44,
-        weeklyAssists: 152,
-        weeklyAvgLifeTime: 790.8953068592058,
-        weeklyDamageDone: 219719,
-        weeklyDamageTaken: 49748,
-        weeklyDeaths: 185,
-        weeklyDistanceTraveled: 31669887.771000005,
-        weeklyHeadshotPercentage: 0.24155405405405406,
-        weeklyKdRatio: 2.13,
-        weeklyKills: 390,
-        weeklyKillsPerGame: 6.434782608695652,
-        weeklyMatchesPlayed: 120,
-        weeklyWallbangs: 0,
-      },
-      rank: "platinum",
-      username: "Anders1337",
-      weeklyKdRatioTrend: 1,
-      dateOfBirth: "1993-05-01",
-    },
-  ];
-
   const getTimeout = (index: number) => {
     return (index + 1) * 100;
   };
@@ -91,7 +20,7 @@ const HomeTopSection = () => {
           <Grid container justify="center">
             <div className="top-text-box">
               <span>
-                CHOOSE BETWEEN A WIDE VARIETY OF{" "}
+                CHOOSE BETWEEN A WIDE VARIETY OF
                 <span className="bold">EXCLUSIVE CARDS</span>
               </span>
             </div>
